168d152643229988ab69f2479972972a618ec08a
sohaibalidev <msohaibaliofficial@gmail.com>
2025-08-19 02:08:27 +0500

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,22 @@
+import Navbar from './components/Navbar';
+import Hero from './components/Hero';
+import Features from './components/Features';
+import FAQ from './components/FAQ';
+import NewsLetter from './components/Newsletter';
+import Footer from './components/Footer';
+import './index.css'
+
+function App() {
+  return (
+    <>
+      <Navbar />
+      <Hero />
+      <Features />
+      <FAQ />
+      <NewsLetter />
+      <Footer />
+    </>
+  );
+}
+
+export default App;
