8bab97b4faa15ee8c1daa4bb3cba7e412f06b531
sohaibalidev <msohaibaliofficial@gmail.com>
2025-09-02 04:45:04 +0500

    update

--- src/main.tsx
+++ src/main.tsx
@@ -3,7 +3,8 @@ import { BrowserRouter } from 'react-router-dom';
 import App from './App';
 import './app.css';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
+
 root.render(
   <BrowserRouter>
     <App />
