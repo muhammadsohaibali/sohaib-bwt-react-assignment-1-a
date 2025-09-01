fb233bf8357e690fe2b8c57bc76811fd2d497671
sohaibalidev <msohaibaliofficial@gmail.com>
2025-09-02 04:39:33 +0500

    update

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import { Routes, Route } from 'react-router-dom';
 import Home from './pages/home/';
-import About from './pages/about/';
-import Contact from './pages/contact/';
+import About from './pages/about';
+import Contact from './pages/contact';
 import Navbar from './components/Navbar';
 import Footer from './components/Footer';
 
